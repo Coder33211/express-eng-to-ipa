a2c591bfde27006dadc8ca178285453a3c13460d
Coder33211 <94192304+Coder33211@users.noreply.github.com>
2024-06-02 14:11:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ 
-    extended: true
-}))
+app.use(express.json());
 
 // app.get("/", (req, res) => res.type('html').send(html));
 
