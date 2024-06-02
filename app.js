a4408f3582cc7a453be350867152c2ffbc158ac7
Coder33211 <94192304+Coder33211@users.noreply.github.com>
2024-06-02 14:08:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ 
+    extended: true
+}))
+
 // app.get("/", (req, res) => res.type('html').send(html));
 
 // const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-const server = app.listen(port);
 
 // server.keepAliveTimeout = 120 * 1000;
 // server.headersTimeout = 120 * 1000;
@@ -13,3 +16,5 @@ const server = app.listen(port);
 app.post("/", (req, res) => {
   res.send(req.body);
 });
+
+const server = app.listen(port);
