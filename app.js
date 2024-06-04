1f7e16e097da0276c9c62cc28e902a276a523321
Coder33211 <94192304+Coder33211@users.noreply.github.com>
2024-06-03 17:43:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -31,17 +31,71 @@ async function convertEngToIPA(text) {
   return data;
 }
 
+async function convertTextToImage(text) {
+  let response = await fetch(
+    "https://api-inference.huggingface.co/models/OFA-Sys/small-stable-diffusion-v0",
+    {
+      headers: {
+        Authorization: "Bearer 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"
+      },
+      method: "POST",
+      body: JSON.stringify(text)
+    }
+  ).then((data) => data.json());
+
+  return response;
+}
+
 app.post("/", async (req, res) => {
-  let text = req.body.text;
+  if (req.body.type == "text") {
+    let text = req.body.text;
 
-  let rawIPA = await convertEngToIPA(text);
+    let rawIPA = await convertEngToIPA(text);
+  
+    if (rawIPA) {
+      let filteredIPA = filterIPA(rawIPA);
+      
+      res.send(filteredIPA);
+    } else {
+      res.send(false);
+    }
+  } else if (req.body.type == "img") {
+    let blob = convertTextToImage(req.body.text);
 
-  if (rawIPA) {
-    let filteredIPA = filterIPA(rawIPA);
-    
-    res.send(filteredIPA);
-  } else {
-    res.send(false);
+    let img = new Image();
+    let canvas = document.createElement("Canvas");
+  
+    let data = [];
+  
+    img.addEventListener("load", () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+  
+      let ctx = canvas.getContext("2d");
+  
+      ctx.drawImage(img, 0, 0);
+  
+      let imgSize = 128;
+      let af = img.width / imgSize;
+  
+      for (let h = 0; h < img.height; h += af) {
+        data.push([]);
+  
+        for (let w = 0; w < img.width; w += af) {
+          let imgData = ctx.getImageData(w, h, 1, 1);
+  
+          let red = imgData.data[0];
+          let green = imgData.data[1];
+          let blue = imgData.data[2];
+  
+          data[data.length - 1].push([red, green, blue]);
+        }
+      }
+
+      res.json(data);
+    });
+  
+    img.src = URL.createObjectURL(blob);
   }
 });
 
